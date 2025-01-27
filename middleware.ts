795aa33d97a52757d440c6bf65bc74f8b4975a00
yeabnoah <yeabnoah5@gmail.com>
2025-01-27 19:47:04 +0300

    implimented a middleware

---
+++ middleware.ts
@@ -0,0 +1,27 @@
+import { betterFetch } from "@better-fetch/fetch";
+import type { auth } from "@/lib/auth";
+import { NextResponse, type NextRequest } from "next/server";
+
+type Session = typeof auth.$Infer.Session;
+
+export default async function authMiddleware(request: NextRequest) {
+  const { data: session } = await betterFetch<Session>(
+    "/api/auth/get-session",
+    {
+      baseURL: request.nextUrl.origin,
+      headers: {
+        //get the cookie from the request
+        cookie: request.headers.get("cookie") || "",
+      },
+    }
+  );
+
+  if (!session) {
+    return NextResponse.redirect(new URL("/signin", request.url));
+  }
+  return NextResponse.next();
+}
+
+export const config = {
+  matcher: ["/dashboard"],
+};
